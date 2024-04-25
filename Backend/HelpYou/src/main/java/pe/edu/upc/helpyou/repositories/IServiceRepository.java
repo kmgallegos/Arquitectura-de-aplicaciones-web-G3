54dac845fdd3b276eee5ad28e23c1f4596a773b8
Eduardoooodev69 <u20211g019@upc.edu.pe>
2024-04-25 06:41:28 -0500

    ddddd

---
+++ Backend/HelpYou/src/main/java/pe/edu/upc/helpyou/repositories/IServiceRepository.java
@@ -0,0 +1,20 @@
+package pe.edu.upc.helpyou.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import pe.edu.upc.helpyou.entities.Service;
+
+import java.util.List;
+
+@Repository
+public interface IServiceRepository extends JpaRepository<Service, Integer> {
+    public List<Service> findByNameService(String name);
+
+    @Query(value="SELECT ts.descriptiontservice AS tipo_servicio, COUNT(DISTINCT u.id_user) AS cantidad_usuarios \n" +
+            "FROM subscription s\n" +
+            "INNER JOIN type_service ts ON s.id_service = ts.id_service \n" +
+            "INNER JOIN userr u ON s.id_user = u.id_user \n" +
+            "GROUP BY  ts.descriptiontservice; \n",nativeQuery = true)
+    public List<String[]> UserByServiceDTO();
+}
---
+++ Backend/HelpYou/src/main/java/pe/edu/upc/helpyou/repositories/IUserRepository.java
@@ -0,0 +1,14 @@
+package pe.edu.upc.helpyou.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+import pe.edu.upc.helpyou.entities.Userr;
+
+import java.util.List;
+@EnableJpaRepositories
+@Repository
+public interface IUserRepository extends JpaRepository<Userr, Integer> {
+    public List<Userr>findByDniUser(String dni);
+
+}
