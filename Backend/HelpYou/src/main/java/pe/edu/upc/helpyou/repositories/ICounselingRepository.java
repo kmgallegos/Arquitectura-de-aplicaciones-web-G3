c85056eaeb929fc06e96aa2b82e45d9a585f77f2
Eduardoooodev69 <134257028+Eduardox21@users.noreply.github.com>
2024-04-25 06:39:11 -0500

    Delete Backend directory

--- Backend/HelpYou/src/main/java/pe/edu/upc/helpyou/repositories/ICounselingRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.helpyou.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.helpyou.entities.Counseling;
-
-public interface ICounselingRepository extends JpaRepository<Counseling, Integer> {
-}
