95621d8d5dc59be079358eabd9fe28e35fdbbd28
Naomi Coronel <151475890+Naomixcode@users.noreply.github.com>
2024-04-27 22:28:58 -0500

    Update SubscriptionServiceImplement.java
    Add

--- Backend/HelpYou/src/main/java/pe/edu/upc/helpyou/servicesimplements/SubscriptionServiceImplement.java
+++ Backend/HelpYou/src/main/java/pe/edu/upc/helpyou/servicesimplements/SubscriptionServiceImplement.java
@@ -2,11 +2,13 @@ package pe.edu.upc.helpyou.servicesimplements;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pe.edu.upc.demosi63.dtos.SubscriptionTypeCountDTO;
 import pe.edu.upc.helpyou.entities.Subscription;
 import pe.edu.upc.helpyou.repositories.ISubscriptionRepository;
 import pe.edu.upc.helpyou.servicesinterfaces.ISubscriptionService;
 
 import java.util.List;
+import java.util.ArrayList;
 @Service
 public class SubscriptionServiceImplement implements ISubscriptionService {
     @Autowired
@@ -26,9 +28,19 @@ public class SubscriptionServiceImplement implements ISubscriptionService {
         return sR.findIncomesBySubscriptionStatusAndMonth();
     }
 
-    @Override
-public List<SubscriptionCountDTO> countUsersBySubscriptionType() {
-    return sR.countUsersBySubscriptionType();
-}
+      @Override
+    public List<SubscriptionTypeCountDTO> countUsersBySubscriptionType() {
+        List<Object[]> results = sR.countUsersBySubscriptionType();
+        List<SubscriptionTypeCountDTO> dtos = new ArrayList<>();
+
+        for (Object[] result : results) {
+            SubscriptionTypeCountDTO dto = new SubscriptionTypeCountDTO();
+            dto.setSubscriptionType((String) result[0]);
+            dto.setUserCount(((Number) result[1]).intValue());
+            dtos.add(dto);
+        }
+
+        return dtos;
+    }
 
 }
