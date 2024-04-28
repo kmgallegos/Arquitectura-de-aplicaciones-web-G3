3ec68156ef22f22ec6318eec4711f1d1e229be4f
Naomi Coronel <151475890+Naomixcode@users.noreply.github.com>
2024-04-27 22:22:28 -0500

    Update IUserRepository.java
    Comment

--- Backend/HelpYou/src/main/java/pe/edu/upc/helpyou/repositories/IUserRepository.java
+++ Backend/HelpYou/src/main/java/pe/edu/upc/helpyou/repositories/IUserRepository.java
@@ -1,13 +1,31 @@
 package pe.edu.upc.helpyou.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import pe.edu.upc.helpyou.entities.Userr;
+import org.springframework.transaction.annotation.Transactional;
+import pe.edu.upc.demosi63.entities.Users;
 
-import java.util.List;
-@EnableJpaRepositories
+
 @Repository
-public interface IUserRepository extends JpaRepository<Userr, Integer> {
-    public List<Userr>findByDniUser(String dni);
+public interface IUserRepository extends JpaRepository<Users, Long> {
+    public Users findByUsername(String username);
+
+    //BUSCAR POR NOMBRE
+    @Query("select count(u.username) from Users u where u.username =:username")
+    public int buscarUsername(@Param("username") String nombre);
+
+    //Buscar usuario con la fecha de fin de su suscripcio y su tipo de suscriop
+
+
+
+
+    //INSERTAR ROLES
+    @Transactional
+    @Modifying
+    @Query(value = "insert into roles (rol, user_id) VALUES (:rol, :user_id)", nativeQuery = true)
+    public void insRol(@Param("rol") String authority, @Param("user_id") Long user_id);
+
 }
