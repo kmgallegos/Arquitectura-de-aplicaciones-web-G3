1bea34bdd821bcd526d864dbd599d514ebe4837e
Naomi Coronel <151475890+Naomixcode@users.noreply.github.com>
2024-04-27 22:20:37 -0500

    SubscriptionTypeCountDTO.java
    Add

---
+++ Backend/HelpYou/src/main/java/pe/edu/upc/helpyou/dtos/SubscriptionTypeCountDTO.java
@@ -0,0 +1,22 @@
+package pe.edu.upc.demosi63.dtos;
+
+public class SubscriptionTypeCountDTO {
+    private String subscriptionType;
+    private int userCount;
+
+    public String getSubscriptionType() {
+        return subscriptionType;
+    }
+
+    public void setSubscriptionType(String subscriptionType) {
+        this.subscriptionType = subscriptionType;
+    }
+
+    public int getUserCount() {
+        return userCount;
+    }
+
+    public void setUserCount(int userCount) {
+        this.userCount = userCount;
+    }
+}
