77b8c2e345bc22e47d4fbe000c1acf695cfd1442
Kimberly Marcela Gallegos Guzman <135560691+kmgallegos@users.noreply.github.com>
2024-04-27 21:33:55 -0500

    Delete Backend/HelpYou1 directory
    delete copy

--- Backend/HelpYou1/src/main/java/pe/edu/upc/helpyou/repositories/IForumRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.helpyou.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.helpyou.entities.Forum;
-
-public interface IForumRepository extends JpaRepository<Forum, Integer> {
-}
--- Backend/HelpYou1/src/main/java/pe/edu/upc/helpyou/repositories/INotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.helpyou.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.helpyou.entities.Notification;
-
-public interface INotificationRepository extends JpaRepository<Notification, Integer> {
-}
