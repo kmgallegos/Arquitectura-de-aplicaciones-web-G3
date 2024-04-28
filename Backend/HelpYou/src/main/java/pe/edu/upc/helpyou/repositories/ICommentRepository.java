9683928e6f4d12dbda7ada8f10bd8fd66dc5c2b2
Naomi Coronel <151475890+Naomixcode@users.noreply.github.com>
2024-04-27 22:24:03 -0500

    Update ICommentRepository.java
    Add

--- Backend/HelpYou/src/main/java/pe/edu/upc/helpyou/repositories/ICommentRepository.java
+++ Backend/HelpYou/src/main/java/pe/edu/upc/helpyou/repositories/ICommentRepository.java
@@ -1,7 +1,17 @@
 package pe.edu.upc.helpyou.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.helpyou.entities.Comment;
+import org.springframework.data.jpa.repository.Query;
+import pe.edu.upc.demosi63.entities.Comment;
+
+import java.util.List;
 
 public interface ICommentRepository extends JpaRepository<Comment, Integer> {
+
+    @Query(value = "SELECT u.username AS username," +
+            " COUNT(c.idComment) AS comment_count " +
+            "FROM Users u JOIN Comment c ON u.id = c.userr.id " +
+            "GROUP BY u.username", nativeQuery = true)
+    List<Object[]> countCommentsByUser();
+
 }
